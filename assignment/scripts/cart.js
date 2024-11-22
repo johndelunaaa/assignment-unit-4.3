3443f114773011b1c742e538ea2965fe549fe534
Johnny De Luna <johndelunaaa@gmail.com>
2024-11-21 21:08:03 -0600

    updated js file

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,46 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+console.log('Basket:', basket);
+
+// let Items = ['kale', 'spinach', 'tacos', 'burritos'];
+// for (let index = 0; index < Items.length; index++) {
+//  addItem;
+// }
+
+function addItem(item) {
+    basket.push(item);
+    console.log('addItems:', item);
+    return true;
+}
+
+addItem('Pizza')
+
+addItem('Spaghetti')
+
+addItem('Tacos')
+
+addItem('Onions')
+
+console.log(basket);
+
+function listItems() {
+for (let index = 0; index < basket.length; index++) {
+    console.log(basket[index]); 
+}
+}
+listItems();
+
+function empty() {
+    basket.length = 0;
+    console.log(basket);
+}
+
+// let basket = []
+
+
+
 
 
 
